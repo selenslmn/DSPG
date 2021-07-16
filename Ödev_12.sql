25040c91cc80b9838fbb8ddf6a625ee05bdc6bb9
selenslmn <56594127+selenslmn@users.noreply.github.com>
2021-07-16 12:46:00 +0300

    Update Ödev_12.sql

--- Ödev_12.sql
+++ Ödev_12.sql
@@ -11,6 +11,8 @@ SELECT * FROM film
 WHERE rental_rate = (SELECT MIN(rental_rate) FROM film) AND replacement_cost = (SELECT MIN(replacement_cost) FROM film);
 
 4.
-SELECT payment.customer_id, SUM(amount) FROM payment
-GROUP BY payment.customer_id
+SELECT SUM(amount), customer.first_name, customer.last_name 
+FROM payment
+JOIN customer ON customer.customer_id = payment.customer_id
+GROUP BY  payment.customer_id, customer.first_name, customer.last_name
 ORDER BY SUM(amount) DESC;
